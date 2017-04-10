01e6d7ccd612a48031030f91442bc538b41b778f
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-04-10 13:59:40 -0600

    test.py add test_create_grocery method

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ from grocerystats import Grocerystats
 import unittest
 
 class GrocerystatsTest(unittest.TestCase):
+
+    # test class creation
+    def test_create_grocery(self):
+        grocery = Grocerystats('vegetable')
+        self.assertEqual(grocery.type, 'vegetable')
+        self.assertEqual(grocery.total_sales, 0)
+        self.assertEqual(grocery.total_quantity, 0)
+
