9e0e1dca4fbe0ff5cc71c6751913a128af8da913
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-04-10 14:00:36 -0600

    test.py add test_add_sale method

--- test.py
+++ test.py
@@ -10,3 +10,10 @@ class GrocerystatsTest(unittest.TestCase):
         self.assertEqual(grocery.total_sales, 0)
         self.assertEqual(grocery.total_quantity, 0)
 
+    def test_add_sale(self):
+        grocery = Grocerystats('fruit')
+        grocery.add_sale()
+        grocery.add_sale()
+
+        self.assertEqual(grocery.total_sales, 2)
+
