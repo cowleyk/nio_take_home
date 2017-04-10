cf19931ded2a2de611ba027557009b422d8a4dd9
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-04-10 14:12:10 -0600

    test.py add test_cart_isolation method

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ class GrocerystatsTest(unittest.TestCase):
         self.assertEqual(grocery.total_sales, 0)
         self.assertEqual(grocery.total_quantity, 0)
 
+    # test add_sale method
     def test_add_sale(self):
         grocery = Grocerystats('fruit')
         grocery.add_sale()
@@ -17,9 +18,37 @@ class GrocerystatsTest(unittest.TestCase):
 
         self.assertEqual(grocery.total_sales, 2)
 
+    # test add_quantity method
     def test_add_quantity(self):
         grocery = Grocerystats('fruit')
         grocery.add_quantity()
         grocery.add_quantity()
 
         self.assertEqual(grocery.total_quantity, 2)
+
+    # not testing message string to json conversion, assuming json.loads() works
+
+    # test 'cart' isolation
+    def test_cart_isolation(self):
+
+        # from sample data in problem's .pdf
+        message_json = {
+            "shopper": {
+                "name": "John Smith",
+                "gender": "male"
+            },
+            "amount": 47.12,
+            "cart": [
+                {
+                    "name": "apple",
+                    "type": "fruit",
+                    "quantity": 3
+                }, {
+                }]
+        }
+
+        self.assertEqual(message_json['cart'], [{"name": "apple",
+                    "type": "fruit",
+                    "quantity": 3}, {}])
+
+
